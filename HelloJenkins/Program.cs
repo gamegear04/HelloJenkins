e1b8e201ac329de6307bff7cd515a8518158037d
Gamegear04 <darius_korzeniewski@web.de>
2018-08-03 15:30:28 +0200

    Start Project

---
+++ HelloJenkins/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloJenkins
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(creatMessage());
+        }
+
+        public static String creatMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloJenkins;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World", Program.creatMessage());
+        }
+    }
+}
